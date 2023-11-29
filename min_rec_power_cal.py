0b2d65b1a9fc8c90414a1f5c5beaa10c825200fe
Kyungsun Lee <103324826+kyungsun-lee-work@users.noreply.github.com>
2023-11-28 23:13:50 -0500

    Create min_rec_power_cal.py

---
+++ min_rec_power_cal.py
@@ -0,0 +1,13 @@
+
+SNR_dB = 20                    #% Desired SNR in dB
+B = 500000                     # % Bandwidth in Hz
+BS_Noise_Power_dBm = -100      #% BS equivalent noise power in dBm
+
+#% Calculate noise power in linear scale
+N_BS = 10**(BS_Noise_Power_dBm / 10)
+
+#% Calculate minimum received power
+P_received = SNR_dB * N_BS * B
+
+#% Display the result
+print('Minimum Received Power at the BS:', P_received)
